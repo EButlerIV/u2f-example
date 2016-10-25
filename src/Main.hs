530d2f71ce75cc05d1307e55e844c092958fc33b
Eugene Butler <eugene@eugene4.com>
2016-10-24 23:04:42 -0400

    split up requests, successful registrations

--- src/Main.hs
+++ src/Main.hs
@@ -46,9 +46,9 @@ data UserSignin = UserSignin {
 } deriving (Show, Generic)
 instance FromJSON UserSignin
 
-data SavedRequest = SavedRequest {
+data SavedRegistration = SavedRegistration {
   request :: Request,
-  savedKeyHandle :: Maybe BS.ByteString,
+  savedKeyHandle :: BS.ByteString,
   identifier :: Maybe String
 } deriving (Show, Generic)
 
@@ -57,10 +57,13 @@ data Identifier = Identifier {
 } deriving (Show, Generic)
 instance ToJSON Identifier
 
--- | RequestList, an in-memory list of all our requests.
---   (never cleared, will blow up indefinitely)
-requestList :: [SavedRequest]
-requestList = []
+-- | RequestCache, an in-memory list of outstanding requests.
+requestCache :: [Request]
+requestCache = []
+
+-- | SavedRegistrationList, in-memory list of keys successfully registered
+registeredKeyList :: [SavedRegistration]
+registeredKeyList = []
 
 -- | Not actually a good random thing generator.
 --   Good enough for demo app purposes.
@@ -70,14 +73,14 @@ randomChallenge = do
   return $ formatOutputBase64 $ LBS.toStrict $ DB.encode randInt
 
 -- findRegistration
-findRequestByChallenge :: [SavedRequest] -> T.Text -> Either U2FError SavedRequest
+findRequestByChallenge :: [Request] -> T.Text -> Either U2FError Request
 findRequestByChallenge requestList chall = case (foundRequest) of
       Just request -> Right request
       Nothing -> Left ChallengeMismatchError
-    where foundRequest = DL.find (\x -> challenge (Main.request x) == chall) requestList
+    where foundRequest = DL.find (\x -> challenge x == chall) requestList
 
-findRequestByIdentifier :: [SavedRequest] -> String -> Either U2FError SavedRequest
-findRequestByIdentifier requestList ident = case (foundRequest) of
+findRegistrationByIdentifier :: [SavedRegistration] -> String -> Either U2FError SavedRegistration
+findRegistrationByIdentifier requestList ident = case (foundRequest) of
       Just request -> Right request
       Nothing -> Left SigninParseError
     where foundRequest = DL.find (\x -> (identifier x) == Just ident) requestList
@@ -92,7 +95,7 @@ parseUserSignin x = case (Data.Aeson.decode  x :: Maybe UserSignin) of
   Just response -> Right response
   Nothing -> Left ChallengeMismatchError
 
-routes rList = do
+routes rCache rSaved = do
   -- Static stuff routes
   get "/" $ file "static/example.html"
   get "/single" $ file "static/single-auth-example.html"
@@ -105,77 +108,80 @@ routes rList = do
   get "/request" $ do
     chall <- liftIO $ randomChallenge
     request <- pure $ Request (T.pack "https://localhost:4000") (T.pack "U2F_V2") chall Nothing
-    written <- liftIO $ modifyIORef rList (++ [(SavedRequest request Nothing Nothing)])
+    written <- liftIO $ modifyIORef rCache (++ [request])
     json $ request
   -- | Endpoints for username-based registration
   post "/user-register" $ do
     registerJSON <- body
-    requests <- liftIO $ readIORef rList
+    requests <- liftIO $ readIORef rCache
     possibleRegistration <- return $ do
       userRegistration <- parseUserRegistration registerJSON
       registration <- pure $ registration userRegistration
       request <- findRequestByChallenge requests (registration_challenge $ registration)
-      verifiedReg <- verifyRegistration (Main.request request) registration
+      verifiedReg <- verifyRegistration request registration
       registrationData <- parseRegistrationData $ TE.encodeUtf8 $ registration_registrationData registration
       return (verifiedReg, (registrationData_publicKey registrationData), (username userRegistration) ++ (password userRegistration))
     case (possibleRegistration) of
       Right (r, pkey, identifier) -> do
-        liftIO $ modifyIORef rList (++ [SavedRequest r (Just pkey) (Just identifier)])
+        liftIO $ modifyIORef rSaved (++ [SavedRegistration r  pkey (Just identifier)])
         json r
       Left err -> do
         raise $ TL.pack (show err)
   post "/user-signin" $ do
     signinJSON <- body
-    requests <- liftIO $ readIORef rList
+    requests <- liftIO $ readIORef rCache
+    registrations <- liftIO $ readIORef rSaved
     possibleSignin <- return $ do
       userSignin <- parseUserSignin signinJSON
       signin <- pure $ signin userSignin
       currentRequest <- findRequestByChallenge requests (clientData_challenge $ fromRight $ parseClientData $ TE.encodeUtf8 $ signin_clientData signin)
-      originalRequest <- findRequestByIdentifier requests ((user userSignin) ++ (pass userSignin))
-      return $ verifySignin (fromJust $ savedKeyHandle originalRequest) (Main.request currentRequest) signin
+      originalRequest <- findRegistrationByIdentifier registrations ((user userSignin) ++ (pass userSignin))
+      return $ verifySignin (savedKeyHandle originalRequest) currentRequest signin
     case (possibleSignin) of
       Right _ -> json $ TL.pack "{status: 'ok'}"
       Left err -> raise $ TL.pack (show err)
   -- | Endpoints for username-based registration
   post "/single-register" $ do
     registerJSON <- body
-    requests <- liftIO $ readIORef rList
+    requests <- liftIO $ readIORef rCache
     possibleRegistration <- return $ do
       registration <- parseRegistration $ LBS.unpack $ registerJSON
       request <- findRequestByChallenge requests (registration_challenge $ registration)
-      verifiedReg <- verifyRegistration (Main.request request) registration
+      verifiedReg <- verifyRegistration request registration
       registrationData <- parseRegistrationData $ TE.encodeUtf8 $ registration_registrationData registration
       return (verifiedReg, (registrationData_publicKey registrationData), (T.unpack $ formatOutputBase64 $ registrationData_certificate registrationData))
     case (possibleRegistration) of
       Right (r, pkey, identifier) -> do
-        liftIO $ modifyIORef rList (++ [SavedRequest r (Just pkey) (Just identifier)])
+        liftIO $ modifyIORef rSaved (++ [SavedRegistration r pkey (Just identifier)])
         json $ Identifier $ identifier
       Left err -> do
         raise $ TL.pack (show err)
   post "/single-signin" $ do
     signinJSON <- body
-    requests <- liftIO $ readIORef rList
+    requests <- liftIO $ readIORef rCache
+    registrations <- liftIO $ readIORef rSaved
     possibleSignin <- return $ do
       userSignin <- parseUserSignin signinJSON
       signin <- pure $ signin userSignin
       currentRequest <- findRequestByChallenge requests (clientData_challenge $ fromRight $ parseClientData $ TE.encodeUtf8 $ signin_clientData signin)
-      originalRequest <- findRequestByIdentifier requests ((user userSignin) ++ (pass userSignin))
-      return $ verifySignin (fromJust $ savedKeyHandle originalRequest) (Main.request currentRequest) signin
+      originalRequest <- findRegistrationByIdentifier registrations ((user userSignin) ++ (pass userSignin))
+      return $ verifySignin (savedKeyHandle originalRequest) currentRequest signin
     case (possibleSignin) of
       Right _ -> json $ TL.pack "{status: 'ok'}"
       Left err -> raise $ TL.pack (show err)
   get "/debug" $ do
-    requests <- liftIO $ readIORef rList
+    requests <- liftIO $ readIORef rSaved
     text $ TL.pack (show requests)
   -- | Route for retrieving keyhandles
   get "/keyhandle" $ do
     identifier <- param "identifier"
-    requests <- liftIO $ readIORef rList
-    case (findRequestByIdentifier requests identifier) of
-      Right request -> json $ TL.fromStrict (fromJust $ keyHandle $ Main.request request)
+    registrations <- liftIO $ readIORef rSaved
+    case (findRegistrationByIdentifier registrations identifier) of
+      Right reg -> json $ TL.fromStrict (fromJust $ keyHandle $ Main.request reg)
       Left _ -> raise "NoKeyHandle"
 
 main :: IO ()
 main = do
-  rList <- newIORef $ requestList
-  scottyTLS 4000 "server.key" "server.crt" (routes rList)
+  requests <- newIORef $ requestCache
+  registered <- newIORef $ registeredKeyList
+  scottyTLS 4000 "server.key" "server.crt" (routes requests registered)
